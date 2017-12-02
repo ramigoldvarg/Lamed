95fb7d85b8df7ae54e5b6425b8a7b57e60d3d5a3
Rami Goldvarg <ramigoldvarg7@gmail.com>
2017-12-02 18:55:25 +0200

    Added a dropdown list

--- client/containers/AddValue.js
+++ client/containers/AddValue.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import DropZone from 'react-dropzone';
 
 class AddValue extends Component {
     constructor() {
         super();
+        this.state = { files: [] };
+        this.onDrop = this.onDrop.bind(this);
+    }
+
+    onDrop(files) {
+        this.setState(prevState=> {
+            return {
+                files: prevState.files.concat(files)
+            }
+        });
     }
 
     render() {
         return (
             <div>
-            הוספת ערך חדש
-            <input type="file" multiple />
+                הוספת ערך חדש
+                <textarea />
+                <DropZone onDrop={this.onDrop}>
+                    <p>מה עם איזה תמונה?</p>
+                </DropZone>
+                <ul>
+                    {
+                        this.state.files.map(file => <li key={file.name}> {file.name} </li>)
+                    }
+                </ul>
             </div>
         );
     }
